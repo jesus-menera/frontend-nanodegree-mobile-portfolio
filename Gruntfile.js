7369ba51fb289ef50fce974194d158478338285e
Jesus Menera <j.menera209@gmail.com>
2015-05-25 18:05:13 -0700

    added grunt minification for css and js, concated google fonts and style into one min file

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,44 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.css'],
+          dest: 'css/dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['css/dist/style.min.css','css/dist/google*.css'],
+        dest: 'css/dist/app.min.css',
+      }
+    },
+    clean: {
+      css: ["css/dist/*.css", "!css/dist/print.min.css","!css/dist/app.min.css"]
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','cssmin','concat','clean']);
+  grunt.registerTask('build', ['uglify','cssmin','concat','clean']);
 
 };
