fd16142263173d38381f0f63b6aa2a60d56173c4
Jesus Menera <j.menera209@gmail.com>
2015-05-25 18:49:04 -0700

    optimizied pizzeria and profilepic, moved css links to footer.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        src: 'js/perfmatters.js',
+        dest: 'js/dist/perfmatters.min.js'
       }
     },
     cssmin: {
