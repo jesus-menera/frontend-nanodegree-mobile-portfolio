8fff13aa0be4bb268e3c13e34a984cc7fefb6e7e
Jesus Menera <j.menera209@gmail.com>
2015-06-20 16:20:38 -0700

    added imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function(grunt) {
 
+  var mozjpeg = require('imagemin-mozjpeg');
+  var pngquant = require('imagemin-pngquant');
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -62,6 +65,12 @@ module.exports = function(grunt) {
       },
       pre_build_clean_css: {
         src: ["css/dist/style.min.css","css/dist/google-font-open-sans.min.css"]
+      },
+      img: {
+        src: ["img/dist/*","img/dist/"]
+      },
+      views_img: {
+        src: ["views/images/dist/*","views/images/dist/"]
       }
     },
     htmlmin: {
@@ -99,8 +108,105 @@ module.exports = function(grunt) {
         files:[
           {expand: true, cwd: 'views/dist/', src: ['**'], dest: 'build/views/'}
         ]
+      },
+      img: {
+        files:[
+          {expand: true, cwd: 'img/dist/', src: ['**'], dest: 'build/img/'}
+        ]
+      },
+      views_img: {
+        files:[
+          {expand: true, cwd: 'views/images/dist/', src: ['**'], dest: 'build/views/images/'}
+        ]
+      }
+    },
+    imagemin: {
+    png: {
+      options: {
+        optimizationLevel: 7
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'img/',
+          src: ['**/*.png'],
+          // Could also match cwd line above. i.e. project-directory/img/
+          dest: 'img/dist/',
+          ext: '.png'
+        }
+      ]
+    },
+    jpg: {
+      options: {
+        progressive: true
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'img/',
+          src: ['**/*.jpg'],
+          // Could also match cwd. i.e. project-directory/img/
+          dest: 'img/dist/',
+          ext: '.jpg'
+        }
+      ]
+    },
+    views_png: {
+      options: {
+        optimizationLevel: 7
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'views/images/',
+          src: ['**/*.png'],
+          // Could also match cwd line above. i.e. project-directory/img/
+          dest: 'views/images/dist/',
+          ext: '.png'
+        }
+      ]
+    },
+    views_jpg: {
+      options: {
+        progressive: true
+      },
+      files: [
+        {
+          // Set to true to enable the following options…
+          expand: true,
+          // cwd is 'current working directory'
+          cwd: 'views/images/',
+          src: ['**/*.jpg'],
+          // Could also match cwd. i.e. project-directory/img/
+          dest: 'views/images/dist/',
+          ext: '.jpg'
+        }
+      ]
+    }
+  }/*
+    imagemin: {
+      static: {                          // Target
+      options: {                       // Target options
+        optimizationLevel: 3,
+        svgoPlugins: [{ removeViewBox: false }],
+        use: [mozjpeg(),pngquant()]
+      },
+      files: {                         // Dictionary of files
+        'img/dist/profilepic.jpg': 'img/profilepic.jpg',
+        'img/dist/2048.png': 'img/2048.png',
+        'img/dist/cam_be_like.jpg': 'img/cam_be_like.jpg',
+        'img/dist/mobilewebdev.jpg': 'img/mobilewebdev.jpg',
+        'views/images/dist/pizza.png': 'views/images/pizza.png'
+        // 'destination': 'source'
       }
     }
+    }*/
   });
 
   // Load the plugin that provides the "uglify" task.
@@ -110,13 +216,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
 
   // Default task(s).
   grunt.registerTask('default', ['uglify','cssmin','concat','clean','htmlmin']);
   grunt.registerTask('build', ['uglify','cssmin','concat','clean']);
   grunt.registerTask('build-copy', ['clean','cssmin','concat','clean:pre_build_clean_css',
-    'uglify','htmlmin','copy','clean:css','clean:html','clean:js','clean:views']);//['copy']
+    'uglify','imagemin','htmlmin','copy','clean:css','clean:html','clean:js','clean:views','clean:img','clean:views_img']);//['copy']
 
 
 };
